3620f29089788e026cf72d3f69c04dfc958d1116
Alvaro Jotar <jotaralvaro@gmail.com>
2026-08-06 11:35:24 -0300

    componente de vista en detalle de heroe con datos

--- src/app/heroes/pages/hero-detail/hero-detail.ts
+++ src/app/heroes/pages/hero-detail/hero-detail.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { HeroService } from '../../../Core/services/hero.service';
+import { HeroUniverse } from '../../../Core/Models/hero.model';
 
 @Component({
   selector: 'app-hero-detail',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+  ],
   templateUrl: './hero-detail.html',
   styleUrl: './hero-detail.css',
 })
-export class HeroDetail {}
+export class HeroDetail {
+  private readonly heroService = inject(HeroService);
+
+  readonly id = input.required<string>();
+
+  readonly hero = computed(() =>
+    this.heroService.getHeroesById(Number(this.id())),
+  );
+
+  universeLogo(universe: HeroUniverse): string {
+    return universe === 'DC' ? 'assets/DC.jpeg' : 'assets/Marvel.jpeg';
+  }
+
+}
