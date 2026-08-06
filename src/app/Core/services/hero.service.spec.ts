96aa5a9e764c9c7bef9053e0e85b1d9d2b2dddaa
Alvaro Jotar <jotaralvaro@gmail.com>
2026-08-06 01:18:24 -0300

    Test de los metodos del heroService

--- src/app/Core/services/hero.service.spec.ts
+++ src/app/Core/services/hero.service.spec.ts
@@ -1,16 +1,82 @@
 import { TestBed } from '@angular/core/testing';
+import { HeroService } from './hero.service';
+import { Hero } from '../Models/hero.model';
+import { SEED_HEROES } from '../Data/seed-heroes';
 
-import { Hero } from './hero';
-
-describe('Hero', () => {
-  let service: Hero;
+describe('HeroService', () => {
+  let service: HeroService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Hero);
+    TestBed.configureTestingModule({
+      providers: [HeroService],
+    });
+    service = TestBed.inject(HeroService);
   });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return all heroes from seed', () => {
+    const heroes = service.getAllHeroes();
+    expect(heroes.length).toBe(SEED_HEROES.length);
+    expect(heroes[0].name).toBe(SEED_HEROES[0].name);
+  })
+
+
+  it('should get a hero by id',() => {
+    const hero = service.getHeroesById(1);
+    expect(hero).toBeDefined();
+    expect(hero?.name).toBe('Spider-Man');
+  })
+
+  it('should return undifined when id does not exist',() => {
+    expect(service.getHeroesById(100)).toBeUndefined();
+  })
+
+  it('should search heroes by name containing term', () => {
+    const result = service.searchHeroesByName('man');
+    const names = result.map((h) => h.name);
+
+    expect(names).toContain('Spider-Man');
+    expect(names).toContain('Superman');
+    expect(names).toContain('Iron Man');
+    expect(names).toContain('Batman');
+  })
+
+  it('should return all heroes when search term is empty', () => {
+    expect(service.searchHeroesByName('').length).toBe(SEED_HEROES.length)
+  })
+
+  it('should create a new hero with generated id', () => {
+    const created = service.createHero({
+      name: 'Aquaman',
+      power: 'Fuerza, durabilidad y agilidad sobrehumanas adaptadas para la vida en las profundidades del océano',
+      universe: 'DC',
+      image: 'assets/heroes/aquaman.jpeg',
+      antagonist: 'Manta Negra'
+    })
+
+    expect(created.id).toBe(SEED_HEROES.length +1);
+    expect(service.getAllHeroes().length).toBe(SEED_HEROES.length + 1);
+    expect(service.getHeroesById(created.id)?.name).toBe('Aquaman');
+  })
+
+  it('should update an existing hero', () => {
+    const original = service.getHeroesById(1);
+    const update: Hero = { ...original!, power: 'Nuevo poder' };
+
+    service.upDateHero(update);
+
+    expect(service.getHeroesById(1)?.power).toBe('Nuevo poder')
+  })
+
+  it('should delete a hero by id', () => {
+    service.deleteHero(1);
+
+    expect(service.getHeroesById(1)).toBeUndefined();
+    expect(service.getAllHeroes().length).toBe(SEED_HEROES.length - 1);
+
+  })
 });
